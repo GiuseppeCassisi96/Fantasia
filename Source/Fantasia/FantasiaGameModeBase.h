d4b6034139c9257d4702c03906c595ed0cd21efb
Giuseppe cassisi <giuseppe.cassisi@studenti.unimi.it>
2022-11-17 11:23:26 +0100

    Init proj

---
+++ Source/Fantasia/FantasiaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FantasiaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FANTASIA_API AFantasiaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
